67c8ee86261d3e9d14c5d9bf4ebb095b3c720c4b
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-01-17 18:23:53 +0800

    urf-config: Add a new gobject for global configuration

---
+++ src/urf-config.c
@@ -0,0 +1,112 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2011 Gary Ching-Pang Lin <glin@novell.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib.h>
+#include "urf-config.h"
+
+#define URF_CONFIG_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), \
+                                     URF_TYPE_CONFIG, UrfConfigPrivate))
+struct UrfConfigPrivate {
+	char *user;
+};
+
+G_DEFINE_TYPE(UrfConfig, urf_config, G_TYPE_OBJECT)
+
+/**
+ * urf_config_load_from_file:
+ **/
+void
+urf_config_load_from_file (UrfConfig  *config,
+			   const char *filename)
+{
+	UrfConfigPrivate *priv = URF_CONFIG_GET_PRIVATE (config);
+	GKeyFile *key_file = g_key_file_new ();
+	gboolean ret = FALSE;
+
+	ret = g_key_file_load_from_file (key_file, filename, G_KEY_FILE_NONE, NULL);
+
+	if (!ret) {
+		g_warning ("Failed to load config file: %s", filename);
+		return;
+	}
+
+	/* Parse the key file and store to private variables*/
+	priv->user = g_key_file_get_value (key_file, "general", "user", NULL);
+
+	g_key_file_free (key_file);
+}
+
+/**
+ * urf_config_get_user:
+ **/
+const char *
+urf_config_get_user (UrfConfig *config)
+{
+	UrfConfigPrivate *priv = URF_CONFIG_GET_PRIVATE (config);
+	return (const char *)priv->user;
+}
+
+/**
+ * urf_config_init:
+ **/
+static void
+urf_config_init (UrfConfig *config)
+{
+	UrfConfigPrivate *priv = URF_CONFIG_GET_PRIVATE (config);
+	priv->user = NULL;
+}
+
+/**
+ * urf_config_finalize:
+ **/
+static void
+urf_config_finalize (GObject *object)
+{
+	UrfConfigPrivate *priv = URF_CONFIG_GET_PRIVATE (object);
+
+	g_free (priv->user);
+}
+
+/**
+ * urf_config_class_init:
+ **/
+static void
+urf_config_class_init(UrfConfigClass *klass)
+{
+	GObjectClass *object_class = (GObjectClass *) klass;
+
+	g_type_class_add_private(klass, sizeof(UrfConfigPrivate));
+	object_class->finalize = urf_config_finalize;
+}
+
+/**
+ * urf_config_new:
+ **/
+UrfConfig *
+urf_config_new (void)
+{
+	UrfConfig *config;
+	config = URF_CONFIG(g_object_new (URF_TYPE_CONFIG, NULL));
+	return config;
+}
---
+++ src/urf-config.h
@@ -0,0 +1,60 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2011 Gary Ching-Pang Lin <glin@novell.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+#ifndef __URF_CONFIG_H__
+#define __URF_CONFIG_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define URF_TYPE_CONFIG (urf_config_get_type())
+#define URF_CONFIG(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), \
+					URF_TYPE_CONFIG, UrfConfig))
+#define URF_CONFIG_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), \
+					URF_TYPE_CONFIG, UrfConfigClass))
+#define URF_IS_CONFIG(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), \
+					URF_TYPE_CONFIG))
+#define URF_IS_CONFIG_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), \
+					URF_TYPE_CONFIG))
+#define URF_GET_CONFIG_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), \
+					URF_TYPE_CONFIG, UrfConfigClass))
+
+typedef struct UrfConfigPrivate UrfConfigPrivate;
+
+typedef struct {
+	GObject parent;
+	UrfConfigPrivate *priv;
+} UrfConfig;
+
+typedef struct {
+        GObjectClass parent_class;
+
+        void (*rf_key_pressed)   (UrfConfig *config, guint code);
+} UrfConfigClass;
+
+GType		 urf_config_get_type 		(void);
+UrfConfig	*urf_config_new			(void);
+void		 urf_config_load_from_file	(UrfConfig	*config,
+						 const char	*filename);
+const char	*urf_config_get_user		(UrfConfig	*config);
+
+G_END_DECLS
+
+#endif /* __URF_CONFIG_H__ */
