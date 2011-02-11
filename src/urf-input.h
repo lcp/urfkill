e7870c3fcf0d957130740f93c0fffaedaecd31f2
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-02-11 17:05:36 +0800

    urf-input: Match input devices by the device names

--- src/urf-input.h
+++ src/urf-input.h
@@ -52,8 +52,7 @@ typedef struct {
 
 GType		 urf_input_get_type 	(void);
 UrfInput	*urf_input_new		(void);
-gboolean	 urf_input_startup	(UrfInput	*input,
-					 const char	*input_devices[]);
+gboolean	 urf_input_startup	(UrfInput	*input);
 
 G_END_DECLS
 
