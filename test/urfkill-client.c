c57e48a0627f10b027e16a69e0511a27e03c75e4
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-03-16 16:38:20 +0800

    Add a test program

---
+++ test/urfkill-client.c
@@ -0,0 +1,98 @@
+#include <time.h>
+#include <urfkill.h>
+#include <stdio.h>
+#include <glib.h>
+
+static GMainLoop *loop = NULL;
+
+static void
+print_urf_killswitch (UrfKillswitch *killswitch)
+{
+	printf ("index = %u\n", urf_killswitch_get_rfkill_index (killswitch));
+	printf ("type  = %u\n", urf_killswitch_get_rfkill_type (killswitch));
+	printf ("state = %d\n", urf_killswitch_get_rfkill_state (killswitch));
+	printf ("soft  = %d\n", urf_killswitch_get_rfkill_soft (killswitch));
+	printf ("hard  = %d\n", urf_killswitch_get_rfkill_hard (killswitch));
+	printf ("name  = %s\n", urf_killswitch_get_rfkill_name (killswitch));
+}
+
+static void
+rfkill_added_cb (UrfClient *client, UrfKillswitch *killswitch, gpointer data)
+{
+	printf ("== Added ==\n");
+	print_urf_killswitch (killswitch);
+	printf ("\n");
+}
+
+static void
+rfkill_removed_cb (UrfClient *client, UrfKillswitch *killswitch, gpointer data)
+{
+	printf ("== removed ==\n");
+	print_urf_killswitch (killswitch);
+	printf ("\n");
+
+	g_object_unref (killswitch);
+}
+
+static void
+rfkill_changed_cb (UrfClient *client, UrfKillswitch *killswitch, gpointer data)
+{
+	printf ("== Changed ==\n");
+	print_urf_killswitch (killswitch);
+	printf ("\n");
+}
+
+static void
+main_sigint_handler (gint sig)
+{
+	signal (SIGINT, SIG_DFL);
+	g_main_loop_quit (loop);
+}
+
+int
+main ()
+{
+	UrfClient *client = NULL;
+	gboolean status;
+	GPtrArray *killswitches;
+	guint i;
+	UrfKillswitch *item;
+	
+
+	g_type_init();
+
+	client = urf_client_new ();
+	status = urf_client_set_wlan_block (client);
+	printf ("Status of block: %d\n", status);
+
+	sleep (2);
+
+	status = urf_client_set_wlan_unblock (client);
+	printf ("Status of unblock: %d\n", status);
+
+	killswitches = urf_client_get_killswitches (client);
+
+	for (i = 0; i<killswitches->len; i++) {
+		item = (UrfKillswitch *)g_ptr_array_index (killswitches, i);
+		print_urf_killswitch (item);
+		printf ("\n");
+	}
+
+	item = urf_client_get_killswitch (client, 1, NULL, NULL);
+
+	printf ("== GetKillswitch ==\n");
+	print_urf_killswitch (item);
+	printf ("\n");
+
+	g_signal_connect (client, "rfkill-added", G_CALLBACK (rfkill_added_cb), NULL);
+	g_signal_connect (client, "rfkill-removed", G_CALLBACK (rfkill_removed_cb), NULL);
+	g_signal_connect (client, "rfkill-changed", G_CALLBACK (rfkill_changed_cb), NULL);
+
+	loop = g_main_loop_new (NULL, FALSE);
+
+	g_main_loop_run (loop);
+
+	g_object_unref (client);
+
+	return 0;
+}
