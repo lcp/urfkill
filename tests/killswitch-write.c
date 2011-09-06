9234df46c08973658672505b7c4f30b5facd3757
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-09-06 10:15:20 +0800

    trivial: Add mainloop to killswitch-write for async block

--- tests/killswitch-write.c
+++ tests/killswitch-write.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <glib.h>
 
+static GMainLoop *loop = NULL;
+
+static void
+main_sigint_handler (gint sig)
+{
+	signal (SIGINT, SIG_DFL);
+	g_main_loop_quit (loop);
+}
+
 int
 main ()
 {
@@ -20,6 +29,12 @@ main ()
 	g_object_set (wlan,
 		      "state", URFSWITCH_STATE_UNBLOCKED,
 		      NULL);
+
+	loop = g_main_loop_new (NULL, FALSE);
+
+	signal (SIGINT, main_sigint_handler);
+
+	g_main_loop_run (loop);
 
 	g_object_unref (wlan);
 
