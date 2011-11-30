497cd0990544fc7014d8cb07039cb31cb6383811
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-11-30 17:44:39 +0800

    urf-polkit: Migrate to gdbus and remove unused code

--- src/urf-polkit.c
+++ src/urf-polkit.c
@@ -26,8 +26,7 @@
 #endif
 
 #include <glib.h>
-#include <dbus/dbus-glib.h>
-#include <dbus/dbus-glib-lowlevel.h>
+#include <gio/gio.h>
 
 #include <polkit/polkit.h>
 
@@ -38,7 +37,7 @@
 
 struct UrfPolkitPrivate
 {
-	DBusGConnection	*connection;
+	GDBusConnection	*connection;
 	PolkitAuthority	*authority;
 };
 
@@ -50,19 +49,19 @@ static gpointer urf_polkit_object = NULL;
  **/
 PolkitSubject *
 urf_polkit_get_subject (UrfPolkit             *polkit,
-			DBusGMethodInvocation *context)
+                        GDBusMethodInvocation *invocation)
 {
-	GError *error;
 	const gchar *sender;
 	PolkitSubject *subject;
 
-	sender = dbus_g_method_get_sender (context);
+	sender = g_dbus_method_invocation_get_sender (invocation);
 	subject = polkit_system_bus_name_new (sender);
 
 	if (subject == NULL) {
-		error = g_error_new (URF_DAEMON_ERROR, URF_DAEMON_ERROR_GENERAL, "failed to get PolicyKit subject");
-		dbus_g_method_return_error (context, error);
-		g_error_free (error);
+		g_dbus_method_invocation_return_error (invocation,
+		                                       URF_DAEMON_ERROR,
+		                                       URF_DAEMON_ERROR_GENERAL,
+		                                       "failed to get PolicyKit subject");
 	}
 
 	return subject;
@@ -75,22 +74,23 @@ gboolean
 urf_polkit_check_auth (UrfPolkit             *polkit,
 		       PolkitSubject         *subject,
 		       const gchar           *action_id,
-		       DBusGMethodInvocation *context)
+		       GDBusMethodInvocation *invocation)
 {
 	gboolean ret = FALSE;
-	GError *error;
-	GError *error_local = NULL;
+	GError *error = NULL;
 	PolkitAuthorizationResult *result;
 
 	/* check auth */
 	result = polkit_authority_check_authorization_sync (polkit->priv->authority,
 							    subject, action_id, NULL,
 							    POLKIT_CHECK_AUTHORIZATION_FLAGS_ALLOW_USER_INTERACTION,
-							    NULL, &error_local);
+							    NULL, &error);
 	if (result == NULL) {
-		error = g_error_new (URF_DAEMON_ERROR, URF_DAEMON_ERROR_GENERAL, "failed to check authorisation: %s", error_local->message);
-		dbus_g_method_return_error (context, error);
-		g_error_free (error_local);
+		g_dbus_method_invocation_return_error (invocation,
+		                                       URF_DAEMON_ERROR,
+		                                       URF_DAEMON_ERROR_GENERAL,
+		                                       "failed to check authorisation: %s",
+		                                       error->message);
 		g_error_free (error);
 		goto out;
 	}
@@ -99,114 +99,14 @@ urf_polkit_check_auth (UrfPolkit             *polkit,
 	if (polkit_authorization_result_get_is_authorized (result)) {
 		ret = TRUE;
 	} else {
-		error = g_error_new (URF_DAEMON_ERROR, URF_DAEMON_ERROR_GENERAL, "not authorized");
-		dbus_g_method_return_error (context, error);
-		g_error_free (error);
+		g_dbus_method_invocation_return_error (invocation,
+		                                       URF_DAEMON_ERROR,
+		                                       URF_DAEMON_ERROR_GENERAL,
+		                                       "not authorized");
 	}
 out:
 	if (result != NULL)
 		g_object_unref (result);
-	return ret;
-}
-
-/**
- * urf_polkit_is_allowed:
- **/
-gboolean
-urf_polkit_is_allowed (UrfPolkit     *polkit,
-		       PolkitSubject *subject,
-		       const gchar   *action_id,
-		       GError       **error)
-{
-	gboolean ret = FALSE;
-	GError *error_local = NULL;
-	PolkitAuthorizationResult *result;
-
-	/* check auth */
-	result = polkit_authority_check_authorization_sync (polkit->priv->authority,
-							    subject, action_id, NULL,
-							    POLKIT_CHECK_AUTHORIZATION_FLAGS_NONE,
-							    NULL, &error_local);
-	if (result == NULL) {
-		g_set_error (error, URF_DAEMON_ERROR, URF_DAEMON_ERROR_GENERAL, "failed to check authorisation: %s", error_local->message);
-		g_error_free (error_local);
-		goto out;
-	}
-
-	ret = polkit_authorization_result_get_is_authorized (result) ||
-	      polkit_authorization_result_get_is_challenge (result);
-out:
-	if (result != NULL)
-		g_object_unref (result);
-	return ret;
-}
-
-/**
- * urf_polkit_get_uid:
- **/
-gboolean
-urf_polkit_get_uid (UrfPolkit     *polkit,
-		    PolkitSubject *subject,
-		    uid_t         *uid)
-{
-	DBusConnection *connection;
-	const gchar *name;
-
-	if (!POLKIT_IS_SYSTEM_BUS_NAME (subject)) {
-		g_debug ("not system bus name");
-		return FALSE;
-	}
-
-	connection = dbus_g_connection_get_connection (polkit->priv->connection);
-	name = polkit_system_bus_name_get_name (POLKIT_SYSTEM_BUS_NAME (subject));
-	*uid = dbus_bus_get_unix_user (connection, name, NULL);
-	return TRUE;
-}
-
-/**
- * urf_polkit_get_pid:
- **/
-gboolean
-urf_polkit_get_pid (UrfPolkit     *polkit,
-		    PolkitSubject *subject,
-		    pid_t         *pid)
-{
-	gboolean ret = FALSE;
-	GError *error = NULL;
-	const gchar *name;
-	DBusGProxy *proxy = NULL;
-
-	/* bus name? */
-	if (!POLKIT_IS_SYSTEM_BUS_NAME (subject)) {
-		g_debug ("not system bus name");
-		goto out;
-	}
-
-	name = polkit_system_bus_name_get_name (POLKIT_SYSTEM_BUS_NAME (subject));
-	proxy = dbus_g_proxy_new_for_name_owner (polkit->priv->connection,
-						 "org.freedesktop.DBus",
-						 "/org/freedesktop/DBus/Bus",
-						 "org.freedesktop.DBus", &error);
-	if (proxy == NULL) {
-		g_warning ("DBUS error: %s", error->message);
-		g_error_free (error);
-		goto out;
-	}
-
-	/* get pid from DBus (quite slow) */
-	ret = dbus_g_proxy_call (proxy, "GetConnectionUnixProcessID", &error,
-				 G_TYPE_STRING, name,
-				 G_TYPE_INVALID,
-				 G_TYPE_UINT, pid,
-				 G_TYPE_INVALID);
-	if (!ret) {
-		g_warning ("failed to get pid: %s", error->message);
-		g_error_free (error);
-		goto out;
-        }
-out:
-	if (proxy != NULL)
-		g_object_unref (proxy);
 	return ret;
 }
 
@@ -221,7 +121,7 @@ urf_polkit_finalize (GObject *object)
 	polkit = URF_POLKIT (object);
 
 	if (polkit->priv->connection != NULL)
-		dbus_g_connection_unref (polkit->priv->connection);
+		g_object_unref (polkit->priv->connection);
 	g_object_unref (polkit->priv->authority);
 
 	G_OBJECT_CLASS (urf_polkit_parent_class)->finalize (object);
@@ -252,7 +152,8 @@ urf_polkit_init (UrfPolkit *polkit)
 
 	polkit->priv = URF_POLKIT_GET_PRIVATE (polkit);
 
-	polkit->priv->connection = dbus_g_bus_get (DBUS_BUS_SYSTEM, &error);
+	/* TODO use g_bus_get */
+	polkit->priv->connection = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, &error);
 	if (polkit->priv->connection == NULL) {
 		if (error != NULL) {
 			g_critical ("error getting system bus: %s", error->message);
