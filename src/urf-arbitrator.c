9193ac11f011ac1a7634dc537cb53e7ae19f54b1
Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
2014-01-13 11:12:40 -0500

    arbitrator: set newly found devices to the saved persistent state
    Signed-off-by: Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>

--- src/urf-arbitrator.c
+++ src/urf-arbitrator.c
@@ -376,6 +376,7 @@ add_killswitch (UrfArbitrator *arbitrator,
 {
 	UrfArbitratorPrivate *priv = arbitrator->priv;
 	UrfDevice *device;
+	KillswitchState state;
 
 	device = urf_arbitrator_find_device (arbitrator, index);
 	if (device != NULL) {
@@ -392,8 +393,25 @@ add_killswitch (UrfArbitrator *arbitrator,
 
 	g_signal_emit (G_OBJECT (arbitrator), signals[DEVICE_ADDED], 0,
 		       urf_device_get_object_path (device));
+
 	if (priv->force_sync && !urf_device_is_platform (device)) {
 		urf_arbitrator_set_block_idx (arbitrator, index, soft);
+	}
+
+	if (priv->persist) {
+		/* If the global state for a killswitch type is not unblocked,
+		 * use the saved persistence state as a default state to
+		 * use for the new killswitch.
+		 *
+		 * This makes sure devices that appear after urfkill has
+		 * started still get to the right state from what was saved
+		 * to the persistence file.
+		 */
+		state = urf_killswitch_get_state (priv->killswitch[type]);
+		if (state != RFKILL_STATE_UNBLOCKED) {
+			soft = urf_config_get_persist_state (priv->config, type);
+			urf_arbitrator_set_block_idx (arbitrator, index, soft);
+		}
 	}
 }
 
